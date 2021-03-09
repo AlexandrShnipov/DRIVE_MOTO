6c4b7978e3189b989f1c7e0bdd4b90f848c95517
Alexandr <sshnipov@gmail.com>
2021-03-09 23:20:17 +0300

    corrected app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.css';
-<<<<<<< Updated upstream
-import Header from './components/header/Header'
-import SecondPage from './components/secondPage/SecondPage'
 
-=======
-import Header from './components/header/'
-import Footer from './components/footer/Footer'
->>>>>>> Stashed changes
+
 
 
 
@@ -16,13 +10,7 @@ import Footer from './components/footer/Footer'
 function App() {
   return ( 
     <div className="App" >
-      <Header/>
-<<<<<<< Updated upstream
-      <SecondPage/>
-=======
-      <Footer/>
-    
->>>>>>> Stashed changes
+     
 
     </div>
   );
