7cd7dbfa1fedaf9f660d294b09376f76e4caaa41
Popova-Julia <juliyapopova007@gmail.com>
2021-03-10 01:20:56 +0300

    breadcrumbList:not(:last-child):after

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header';
-import SecondPage from './components/secondPage/SecondPage';
+
 
 
 
@@ -12,8 +11,6 @@ import SecondPage from './components/secondPage/SecondPage';
 function App() {
   return ( 
     <div className="App" >
-<Header/>
-<SecondPage/>
 
     </div>
   );
