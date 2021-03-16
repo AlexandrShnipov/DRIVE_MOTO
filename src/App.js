68761669601b3af5621ebee742c55b9409d52950
Alexandr <sshnipov@gmail.com>
2021-03-17 01:44:05 +0300

    corrected sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
+<<<<<<< Updated upstream
 import Header from './components/header/Header';
 import SecondPage from './components/secondPage/SecondPage.jsx';
+=======
+import Header from './components/header/'
+import Footer from './components/footer/Footer'
+>>>>>>> Stashed changes
 
 
 
@@ -10,8 +15,14 @@ import SecondPage from './components/secondPage/SecondPage.jsx';
 function App() {
   return ( 
     <div className="App" >
+<<<<<<< Updated upstream
 <Header/>
       <SecondPage />
+=======
+      <Header/>
+      <Footer/>
+    
+>>>>>>> Stashed changes
 
     </div>
   );
