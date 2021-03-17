58f418b46e95e34146e3475cf06579e6d0c29bbc
Popova-Julia <juliyapopova007@gmail.com>
2021-03-17 22:42:58 +0300

    creature third page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import SecondPage from './components/secondPage/SecondPage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import HomePage from './pages/homePage/HomePage';
+import ThirdPage from './components/thirdPage/ThirdPage';
 
 function App() {
   return ( 
     <div className="App" >
       {/* <HomePage/> */}
-      <Header/>
+      {/* <Header/>
           <SecondPage/>
-      <Footer/>
+      <Footer/> */}
+      <ThirdPage/>
     </div>
   );
 }
